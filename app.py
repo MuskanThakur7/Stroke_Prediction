384818df7285712a050bde337946e3867d8f713d
MuskanThakur7 <115647166+MuskanThakur7@users.noreply.github.com>
2023-04-23 20:01:21 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import numpy as np
+import pickle
+
+model = pickle.load(open('model.pkl', 'rb'))
+
+st.title('What? :smoking:')
+
+age = st.slider("Age", 1, 85)
+ever_married = st.selectbox("Married", ["0", "1"])
+bmi = st.slider("BMI", 10.0, 50.0)
+Residence_type = st.selectbox("Residence", ["1", "0"])
+Never_worked = st.radio("Working?", ["1", "0"])
+hypertension = st.selectbox("hypertension", ["1", "0"])
+Govt_job = st.radio("Govt_job?", ["1", "0"])
+heart_disease = st.radio("Govt_job?", ["1", "0"])
+
+def predict():
+    float_features = [float(x) for x in [bmi]]
+    categorical_features = [gender, tartar,ever_married,Residence_type,Never_worked,hypertension,Govt_job,heart_disease]
+    integer_features = [age]
+
+    final_features = [np.array(float_features + integer_features + categorical_features)]
+    prediction = model.predict(final_features)
+    label = prediction[0]
+   
+    print(type(label))
+    print(label)
+
+    st.success('The Prediction is : ' + str(label))
+   
+trigger = st.button('Predict', on_click=predict)
